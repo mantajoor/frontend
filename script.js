c562826cc0f2d21a9dc925c2cd080cb3053b1b6f
mantajoor <mantajoor@gmail.com>
2026-03-29 19:22:46 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeText() {
-  document.getElementById("text").innerText = "O'zgardi";
-}
-function showName() {
-  let name = document.getElementById("name").value;
-  document.getElementById("result").innerText = "Salom, " + name;
-}
