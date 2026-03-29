6d2d7ba12d3fc06e376b678dd9355ff0fb098ee7
mantajoor <mantajoor@gmail.com>
2026-03-29 19:23:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeText() {
+  document.getElementById("text").innerText = "O'zgardi";
+}
+function showName() {
+  let name = document.getElementById("name").value;
+  document.getElementById("result").innerText = "Salom, " + name;
+}
